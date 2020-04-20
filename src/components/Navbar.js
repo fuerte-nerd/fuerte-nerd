424ec9d605b040959420610296f4672084960c8d
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-20 11:51:05 +0100

    Autogit: 20-04-2020 11:51

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,42 @@
 import React from "react"
 
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core"
+
+import { Menu, GitHub, Phone, Email, WhatsApp } from "@material-ui/icons"
+
 const Navbar = () => {
-  return null
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          style={{ marginRight: ".25rem" }}
+        >
+          <Menu />
+        </IconButton>
+        <Typography variant="subtitle1" style={{ marginRight: ".25rem" }}>
+          David Andrews
+        </Typography>
+
+        <Typography style={{ flex: 1 }} variant="subtitle2">
+          (Web Developer)
+        </Typography>
+        <IconButton color="inherit">
+          <GitHub />
+        </IconButton>
+        <IconButton color="inherit">
+          <Phone />
+        </IconButton>
+        <IconButton color="inherit">
+          <Email />
+        </IconButton>
+        <IconButton edge="end" color="inherit">
+          <WhatsApp />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
 export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,43 +4,14 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import { Box, AppBar, Toolbar, Typography, IconButton } from "@material-ui/core"
+import { Box, Toolbar, Typography } from "@material-ui/core"
 
-import { Menu, Phone, Email, WhatsApp, GitHub } from "@material-ui/icons"
+import Navbar from "../components/Navbar"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <AppBar>
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          style={{ marginRight: ".25rem" }}
-        >
-          <Menu />
-        </IconButton>
-        <Typography variant="subtitle1" style={{ marginRight: ".25rem" }}>
-          David Andrews
-        </Typography>
-
-        <Typography style={{ flex: 1 }} variant="subtitle2">
-          (Web Developer)
-        </Typography>
-        <IconButton color="inherit">
-          <GitHub />
-        </IconButton>
-        <IconButton color="inherit">
-          <Phone />
-        </IconButton>
-        <IconButton color="inherit">
-          <Email />
-        </IconButton>
-        <IconButton edge="end" color="inherit">
-          <WhatsApp />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <Navbar />
     <Box>
       <Toolbar />
       <Typography variant="h1">My site</Typography>
