8d23811eb04034424e079e8b4807c68d221c5690
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-20 10:55:28 +0100

    Autogit: 20-04-2020 10:55

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,19 @@ import SEO from "../components/seo"
 
 import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core"
 
+import { MenuOutlined } from "@material-ui/icons"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <AppBar>
+      <Toolbar>
+        <IconButton>
+          <MenuOutlined />
+        </IconButton>
+        <Typography>David Andrews</Typography>
+      </Toolbar>
+    </AppBar>
   </Layout>
 )
 
