883096cbfe9de09475b163362124cea307b6b8b8
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-21 13:36:23 +0100

    Autogit: 21-04-2020 13:36

--- src/components/SectionHome.js
+++ src/components/SectionHome.js
@@ -8,6 +8,8 @@ import {
   Typography,
   Button,
 } from "@material-ui/core"
+
+import { makeStyles } from "@material-ui/core/styles"
 import { DirectionsRun } from "@material-ui/icons"
 import { HeroImage } from "./image"
 import { Link } from "react-scroll"
