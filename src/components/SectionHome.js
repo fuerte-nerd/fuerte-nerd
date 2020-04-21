3b158191d5792331699e9d039cb4f5e1e52c4969
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-21 13:16:02 +0100

    Autogit: 21-04-2020 13:16

--- src/components/SectionHome.js
+++ src/components/SectionHome.js
@@ -1,55 +1,71 @@
 import React from "react"
-import { Box, Container, Grid, Typography, Button } from "@material-ui/core"
+import {
+  Hidden,
+  Toolbar,
+  Box,
+  Container,
+  Grid,
+  Typography,
+  Button,
+} from "@material-ui/core"
 import { DirectionsRun } from "@material-ui/icons"
 import { HeroImage } from "./image"
+import { Link } from "react-scroll"
 
 const SectionHome = () => {
   return (
-    <Box
-      id="home"
-      minHeight="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Container>
-        <Grid container alignItems="center" justify="center">
-          <Grid item xs={12} md={4}>
-            <Box width="100%" align="center">
-              <HeroImage />
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <Typography
-              variant="h2"
-              variantMapping={{ h2: "h1" }}
-              align="center"
-            >
-              Hello!
-            </Typography>
-            <Typography variant="subtitle1" align="center" paragraph>
-              My name is David and I make apps, websites and cups of coffee*
-              with code.
-            </Typography>
-            <Box align="center">
-              <Button
-                variant="contained"
-                size="large"
-                color="primary"
-                startIcon={<DirectionsRun />}
+    <>
+      <Hidden smUp>
+        <Toolbar />
+      </Hidden>
+      <Box
+        id="home"
+        minHeight="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Container>
+          <Grid container alignItems="center" justify="center">
+            <Grid item xs={12} md={4}>
+              <Box width="100%" align="center">
+                <HeroImage />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography
+                variant="h2"
+                variantMapping={{ h2: "h1" }}
+                align="center"
               >
-                Come and see more!
-              </Button>
-            </Box>
-            <Box mt={1} width="100%" display="block" textAlign="center">
-              <Typography variant="caption" align="center">
-                *work in progress
+                Hello!
               </Typography>
-            </Box>
+              <Typography variant="subtitle1" align="center" paragraph>
+                My name is David and I make apps, websites and cups of coffee*
+                with code.
+              </Typography>
+              <Box align="center">
+                <Link to="about" smooth={true}>
+                  <Button
+                    variant="contained"
+                    size="large"
+                    color="primary"
+                    startIcon={<DirectionsRun />}
+                  >
+                    Come and see more!
+                  </Button>
+                </Link>
+              </Box>
+              <Box mt={1} width="100%" display="block" textAlign="center">
+                <Typography variant="caption" align="center">
+                  *work in progress
+                </Typography>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    </Box>
+        </Container>
+      </Box>
+    </>
   )
 }
 
