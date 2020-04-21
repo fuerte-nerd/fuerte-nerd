64b06b4655550b54a62efb4c2fd06b890e752b0c
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-21 12:05:23 +0100

    Autogit: 21-04-2020 12:05

---
+++ src/components/SectionHome.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { Box, Container, Grid, Typography, Button } from "@material-ui/core"
+import { DirectionsRun } from "@material-ui/icons"
+import { HeroImage } from "./image"
+
+const SectionHome = () => {
+  return (
+    <Box
+      minHeight="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Container>
+        <Grid container alignItems="center" justify="center">
+          <Grid item xs={12} md={4}>
+            <Box width="100%" align="center">
+              <HeroImage />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography
+              variant="h2"
+              variantMapping={{ h2: "h1" }}
+              align="center"
+            >
+              Hello!
+            </Typography>
+            <Typography variant="subtitle1" align="center" paragraph>
+              My name is David and I make apps, websites and cups of coffee*
+              with code.
+            </Typography>
+            <Box align="center">
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                startIcon={<DirectionsRun />}
+              >
+                Come and see more!
+              </Button>
+            </Box>
+            <Box mt={3} width="100%" display="block" textAlign="center">
+              <Typography variant="caption" align="center">
+                *work in progress
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  )
+}
+
+export default SectionHome
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,21 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import { HeroImage } from "../components/image"
+//import { HeroImage } from "../components/image"
 import SEO from "../components/seo"
 
-import { Grid, Container, Box, Typography, Button } from "@material-ui/core"
-import { DirectionsRun } from "@material-ui/icons"
+//import { Grid, Container, Box, Typography, Button } from "@material-ui/core"
+//import { DirectionsRun } from "@material-ui/icons"
 import Navbar from "../components/Navbar"
 import NavMenu from "../components/NavMenu"
+import SectionHome from "../components/SectionHome"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Navbar />
     <NavMenu />
-    <Box
-      minHeight="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Container>
-        <Grid container alignItems="center" justify="center">
-          <Grid item xs={12} md={4}>
-            <Box width="100%" align="center">
-              <HeroImage />
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <Typography
-              variant="h2"
-              variantMapping={{ h2: "h1" }}
-              align="center"
-            >
-              Hello!
-            </Typography>
-            <Typography variant="subtitle1" align="center" paragraph>
-              My name is David and I make apps, websites and cups of coffee*
-              with code.
-            </Typography>
-            <Box align="center">
-              <Button
-                variant="contained"
-                size="large"
-                color="primary"
-                startIcon={<DirectionsRun />}
-              >
-                Come and see more!
-              </Button>
-            </Box>
-            <Box mt={3} width="100%" display="block" textAlign="center">
-              <Typography variant="caption" align="center">
-                *work in progress
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+    <SectionHome />
   </Layout>
 )
 
