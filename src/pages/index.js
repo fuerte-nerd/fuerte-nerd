caa4b9d797f213104970493dc1a672caf56a2429
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-21 10:48:41 +0100

    Autogit: 21-04-2020 10:48

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import { Box, Toolbar, Typography } from "@material-ui/core"
+import { Grid, Container, Box, Toolbar, Typography } from "@material-ui/core"
 
 import Navbar from "../components/Navbar"
 import NavMenu from "../components/NavMenu"
@@ -14,9 +14,28 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Navbar />
     <NavMenu />
-    <Box>
-      <Toolbar />
-      <Typography variant="h1">My site</Typography>
+    <Box
+      minHeight="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Container>
+        <Grid container>
+          <Grid item xs={12} md={4}>
+            Image
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Typography variant="h4" variantMapping={{ h4: "h1" }}>
+              Hi!
+            </Typography>
+            <Typography>
+              My name is David and I make apps, websites and cups of tea* with
+              code.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   </Layout>
 )
