9ba2a3f12efa7a0c5f22149701a54cfca4f59e84
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-21 13:41:28 +0100

    Autogit: 21-04-2020 13:41

---
+++ src/components/NavMenuItem.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { connect } from "react-redux"
+import { toggleMenu } from "../redux/actions"
+
+const NavMenuItem = props => {
+  return (
+    <Link to={props.link} smooth={true} offset={0}>
+      <ListItem
+        button
+        alignItems="center"
+        id={`${props.link}Link`}
+        onClick={() => props.dispatch(toggleMenu(!props.isOpen))}
+      >
+        <ListItemText
+          primaryTypographyProps={{ variant: "button", align: "center" }}
+        >
+          {props.label}
+        </ListItemText>
+      </ListItem>
+    </Link>
+  )
+}
+
+mapStateToProps = state => ({
+  isOpen: state.navOpen,
+})
+export default connect(mapStateToProps)(NavMenuItem)
