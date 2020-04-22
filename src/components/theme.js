0efee860f39ddff8c10a04ee620ec3c01af4b7f0
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-22 14:07:09 +0100

    Autogit: 22-04-2020 14:07

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,9 +1,10 @@
-import { teal, yellow } from "@material-ui/core/colors"
+import { blueGrey, yellow } from "@material-ui/core/colors"
 import { createMuiTheme } from "@material-ui/core/styles"
 
 export default createMuiTheme({
   palette: {
-    primary: teal,
+    primary: blueGrey,
     secondary: yellow,
+    tonalOffset: 0.4,
   },
 })
