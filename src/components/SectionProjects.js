38429beecdd5c9cbea008136b71709a889ff2b98
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-22 12:35:29 +0100

    Autogit: 22-04-2020 12:35

--- src/components/SectionProjects.js
+++ src/components/SectionProjects.js
@@ -6,13 +6,22 @@ import { HeroImage } from "./image"
 const SectionProjects = () => {
   return (
     <Box
+      py={4}
       id="projects"
       minHeight="100vh"
       display="flex"
       justifyContent="center"
       alignItems="center"
     >
-      <Container>Projects</Container>
+      <Container>
+        <Box>
+          <Typography variant="h2">My Work</Typography>
+          <Typography variant="subtitle1">
+            Here you will find a selection of my work ranging from SPAs (Single
+            Page Applications) to websites (and more!)...
+          </Typography>
+        </Box>
+      </Container>
     </Box>
   )
 }
