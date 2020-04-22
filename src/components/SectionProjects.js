933070cc2a21a83bac7ade2cc5e250dfd88e426f
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-22 12:40:37 +0100

    Autogit: 22-04-2020 12:40

--- src/components/SectionProjects.js
+++ src/components/SectionProjects.js
@@ -1,5 +1,17 @@
 import React from "react"
-import { Box, Container, Grid, Typography, Button } from "@material-ui/core"
+import {
+  Box,
+  Card,
+  CardMedia,
+  CardHeader,
+  CardContent,
+  CardActions,
+  CardActionArea,
+  Container,
+  Grid,
+  Typography,
+  Button,
+} from "@material-ui/core"
 import { DirectionsRun } from "@material-ui/icons"
 import { HeroImage } from "./image"
 
@@ -17,10 +29,81 @@ const SectionProjects = () => {
         <Box>
           <Typography variant="h2">My Work</Typography>
           <Typography variant="subtitle1">
-            Here you will find a selection of my work ranging from SPAs (Single
+            Here you will find a selection of my work, ranging from SPAs (Single
             Page Applications) to websites (and more!)...
           </Typography>
         </Box>
+        <Grid container>
+          <Grid item>
+            <Card>
+              <CardActionArea>
+                <CardMedia></CardMedia>
+                <CardContent>
+                  <Typography variant="h5">Todos</Typography>
+                  <Typography variant="body2">
+                    A React application I built using Redux for state management
+                    and Material UI for styling
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button>Visit</Button>
+                <Button>Code</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid>
+            <Card>
+              <CardActionArea>
+                <CardMedia></CardMedia>
+                <CardContent>
+                  <Typography></Typography>
+                  <Typography></Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button></Button>
+                <Button></Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid>
+            <Card>
+              <CardActionArea>
+                <CardMedia></CardMedia>
+                <CardContent>
+                  <Typography></Typography>
+                  <Typography></Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button></Button>
+                <Button></Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid>
+            <Card>
+              <CardActionArea>
+                <CardMedia></CardMedia>
+                <CardContent>
+                  <Typography></Typography>
+                  <Typography></Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button></Button>
+                <Button></Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   )
