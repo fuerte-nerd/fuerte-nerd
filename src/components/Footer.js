af40f76af6c3e5b7d3752c157c0e7538fbf09a5e
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-23 21:37:08 +0100

    Autogit: 23-04-2020 21:37

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import { Box, Container, Typography } from "@material-ui/core"
 
 const Footer = () => {
   return (
-    <Box bgcolor="primary.main" boxShadow={3} py={4} align="center">
+    <Box
+      bgcolor="primary.main"
+      color="#fff"
+      boxShadow={3}
+      py={4}
+      align="center"
+    >
       <Container>
         <Typography variant="caption">
           All content &copy; 2020
