21a84d935775fe7ee75acb05454ecd181ec674fa
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-23 21:21:53 +0100

    Autogit: 23-04-2020 21:21

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Box, Typography } from "@material-ui/core"
+import { Box, Container, Typography } from "@material-ui/core"
 
 const Footer = () => {
   return (
-    <Box py={4}>
-      <Typography>Hello</Typography>
+    <Box py={4} align="center">
+      <Container>
+        <Typography variant="caption">
+          All content &copy; 2020
+          {new Date().getFullYear() === 2020
+            ? null
+            : `-${new Date().getFullYear()}`}
+          {` `} David Andrews
+        </Typography>
+      </Container>
     </Box>
   )
 }
