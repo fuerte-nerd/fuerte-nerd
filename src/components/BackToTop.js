d3607e743bfb5a13cd6fd80d00b5eed14a5a7df4
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-23 14:41:56 +0100

    Autogit: 23-04-2020 14:41

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { connect } from "react-redux"
-import { Fade, Fab } from "@material-ui/core"
+import { Fade, Fab, Tooltip } from "@material-ui/core"
 import { ArrowUpward } from "@material-ui/icons"
 import { Link } from "react-scroll"
 
@@ -8,13 +8,15 @@ const BackToTop = props => {
   return (
     <Fade in={props.backToTopVisible}>
       <Link to="home" smooth={true}>
-        <Fab
-          color="primary"
-          size="small"
-          style={{ position: "fixed", bottom: "1rem", right: "1rem" }}
-        >
-          <ArrowUpward />
-        </Fab>
+        <Tooltip title="Back to top" placement="left">
+          <Fab
+            color="secondary"
+            size="small"
+            style={{ position: "fixed", bottom: "1rem", right: "1rem" }}
+          >
+            <ArrowUpward />
+          </Fab>
+        </Tooltip>
       </Link>
     </Fade>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = props => {
     }
     window.addEventListener("scroll", onScroll)
     return () => window.removeEventListener("scroll", onScroll)
-  }, [props.isSmoothScrolling])
+  }, [props.isSmoothScrolling, compState.timer])
 
   return (
     <Layout>
