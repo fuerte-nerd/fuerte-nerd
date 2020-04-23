912d5048c20ecd4fff2f18dfa6b4892f32572352
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-23 12:59:55 +0100

    Autogit: 23-04-2020 12:59

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { Fab } from "@material-ui/core"
+import { Home } from "@material-ui/icons"
 
 const BackToTop = () => {
-  return <Fab>^</Fab>
+  return (
+    <Fab style={{ position: "fixed", bottom: "2.5rem", right: "2.5rem" }}>
+      <Home />
+    </Fab>
+  )
 }
 
 export default BackToTop
