68c4575d67820737d8c68e2cae1f4f0dc75cd2f3
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-24 23:59:13 +0100

    Autogit: 24-04-2020 23:59

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,24 @@
-import { SET_MENU, SET_BACK_TO_TOP, SET_SMOOTH_SCROLLING } from "./types"
+import {
+  SET_MENU,
+  SET_BACK_TO_TOP,
+  SET_SMOOTH_SCROLLING,
+  SET_CALL_MENU_ANCHOR,
+  SET_TEXT_MENU_ANCHOR,
+} from "./types"
 
 export const toggleMenu = menuState => ({
   type: SET_MENU,
   payload: menuState,
+})
+
+export const setCallMenuAnchor = element => ({
+  type: SET_CALL_MENU_ANCHOR,
+  payload: element,
+})
+
+export const setTextMenuAnchor = element => ({
+  type: SET_TEXT_MENU_ANCHOR,
+  payload: element,
 })
 
 export const toggleBackToTop = buttonState => ({
