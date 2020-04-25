1e27878084303a77368ddf6b4d869d70f40e913d
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-25 14:49:35 +0100

    Autogit: 25-04-2020 14:49

--- src/components/WhatsAppMenu.js
+++ src/components/WhatsAppMenu.js
@@ -24,13 +24,13 @@ const WhatsAppMenu = props => {
       onClose={() => props.dispatch(setWhatsAppMenuAnchor(null))}
     >
       <MenuItem>
-        Call on WhatsApp
+        Call me on WhatsApp
         <Typography variant="caption" style={{ marginLeft: 10 }}>
           ({data.site.siteMetadata.contact.phoneStr})
         </Typography>
       </MenuItem>
       <MenuItem>
-        Chat on WhatsApp
+        Message me on WhatsApp
         <Typography variant="caption" style={{ marginLeft: 10 }}>
           ({data.site.siteMetadata.contact.phoneStr})
         </Typography>
