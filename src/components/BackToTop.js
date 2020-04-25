46424d329ce917907de340a0fefaee1679ad163d
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-25 09:40:58 +0100

    Autogit: 25-04-2020 09:40

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { connect } from "react-redux"
+import { setSmoothScroll } from "../redux/actions"
 import { Fade, Fab, Tooltip } from "@material-ui/core"
 import { ArrowUpward } from "@material-ui/icons"
-import { Link } from "react-scroll"
+import { Link, Events } from "react-scroll"
 
 const BackToTop = props => {
+  useEffect(() => {
+    Events.scrollEvent.register("begin", () =>
+      props.dispatch(setSmoothScroll(true))
+    )
+    Events.scrollEvent.register("end", () =>
+      props.dispatch(setSmoothScroll(false))
+    )
+  }, [])
+  const handleHover = () => {
+    clearTimeout(props.backToTopTimer)
+  }
   return (
     <Fade in={props.backToTopVisible}>
-      <Link to="home" smooth={true}>
+      <Link to="home" smooth={true} onMouseOver={handleHover}>
         <Tooltip title="Back to top" placement="left">
           <Fab
             color="primary"
@@ -24,6 +36,7 @@ const BackToTop = props => {
 
 const mapStateToProps = state => ({
   backToTopVisible: state.backToTopVisible,
+  backToTopTimer: state.backToTopTimer,
 })
 
 export default connect(mapStateToProps)(BackToTop)
