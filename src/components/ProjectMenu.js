e078a838137fdee9d48683fba54a91db27cd1ce8
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-25 16:46:22 +0100

    Autogit: 25-04-2020 16:46

--- src/components/ProjectMenu.js
+++ src/components/ProjectMenu.js
@@ -1,54 +1,24 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import { connect } from "react-redux"
-import { setWhatsAppMenuAnchor } from "../redux/actions"
+import { setProjectMenuAnchor } from "../redux/actions"
 import { Menu, MenuItem, Typography } from "@material-ui/core"
 
-const WhatsAppMenu = props => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          contact {
-            phone
-            phoneStr
-          }
-        }
-      }
-    }
-  `)
+const ProjectMenu = props => {
   return (
     <Menu
-      anchorEl={props.whatsAppMenuAnchorEl}
-      open={Boolean(props.whatsAppMenuAnchorEl)}
-      onClose={() => props.dispatch(setWhatsAppMenuAnchor(null))}
+      anchorEl={props.projectMenuAnchorEl}
+      open={Boolean(props.projectMenuAnchorEl)}
+      onClose={() => props.dispatch(setProjectMenuAnchor(null))}
     >
-      <MenuItem>
-        Call on WhatsApp
-        <Typography
-          color="textSecondary"
-          variant="caption"
-          style={{ marginLeft: 10 }}
-        >
-          ({data.site.siteMetadata.contact.phoneStr})
-        </Typography>
-      </MenuItem>
-      <MenuItem>
-        Send WhatsApp
-        <Typography
-          color="textSecondary"
-          variant="caption"
-          style={{ marginLeft: 10 }}
-        >
-          ({data.site.siteMetadata.contact.phoneStr})
-        </Typography>
-      </MenuItem>
+      <MenuItem>Visit Project</MenuItem>
+      <MenuItem>View Code</MenuItem>
     </Menu>
   )
 }
 
 const mapStateToProps = state => ({
-  whatsAppMenuAnchorEl: state.whatsAppMenuAnchorEl,
+  projectMenuAnchorEl: state.projectMenuAnchorEl,
 })
 
-export default connect(mapStateToProps)(WhatsAppMenu)
+export default connect(mapStateToProps)(ProjectMenu)
