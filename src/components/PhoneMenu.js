dbb774aa5c6bbdfccc46d3eb634af09dd83e94af
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-25 12:37:13 +0100

    Autogit: 25-04-2020 12:37

---
+++ src/components/PhoneMenu.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { connect } from "react-redux"
+import { setCallMenuAnchor } from "../redux/actions"
+import { Menu, MenuItem, ListItemIcon } from "@material-ui/core"
+
+import { Phone, WhatsApp } from "@material-ui/icons"
+
+const CallMenu = props => {
+  return (
+    <Menu
+      anchorEl={props.callMenuAnchorEl}
+      open={Boolean(props.callMenuAnchorEl)}
+      onClose={() => props.dispatch(setCallMenuAnchor(null))}
+    >
+      <MenuItem>Call on phone</MenuItem>
+      <MenuItem>Call on WhatsApp</MenuItem>
+    </Menu>
+  )
+}
+
+const mapStateToProps = state => ({
+  callMenuAnchorEl: state.callMenuAnchorEl,
+})
+
+export default connect(mapStateToProps)(CallMenu)
