1bc0f150ef9cfaca3ef2596dbbfd43f872acee0c
David Andrews <dandrewsuk82@gmail.com>
2020-04-27 00:06:25 +0100

    Autogit: 27-04-2020 00:06

--- src/components/OGTags.js
+++ src/components/OGTags.js
@@ -9,10 +9,10 @@ const OGTags = () => {
         property="og:description"
         content={data.site.siteMetadata.description}
       />
-      <meta
-        property="og:description"
-        content={data.site.siteMetadata.description}
-      />
+      <meta property="og:url" content={data.site.siteMetadata.url} />
+      <meta property="og:image" content={data.site.siteMetadata.ogImage} />
+      <meta property="og:site_name" content={data.site.siteMetadata.title} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   )
 }
