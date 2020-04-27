bcf0dd1dfc909fb8542d353fd934eaae0975b106
David Andrews <dandrewsuk82@gmail.com>
2020-04-27 13:50:53 +0100

    Autogit: 27-04-2020 13:50

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ import { Link } from "react-scroll"
 import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core"
 
 import { Menu, Instagram, GitHub, Phone, Email } from "@material-ui/icons"
+import { Fade } from "react-reveal"
 
 import NavbarIconButton from "./NavbarIconButton"
 import CallMenu from "./CallMenu"
@@ -78,61 +79,63 @@ const Navbar = props => {
 
   return (
     <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            style={{ marginRight: ".25rem" }}
-            onClick={handleClick}
-            id="menu"
-          >
-            <Menu />
-          </IconButton>
-          <Link to="home" smooth={true}>
-            <Typography
-              variant="h6"
-              variantMapping={{ h6: "h1" }}
-              style={{ cursor: "pointer" }}
+      <Fade down>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              style={{ marginRight: ".25rem" }}
+              onClick={handleClick}
+              id="menu"
             >
-              {title}
-            </Typography>
-          </Link>
-          <span style={{ flex: 1 }} />
-          <NavbarIconButton
-            hideOnSmallDevices={true}
-            clickHandler={handleClick}
-            tooltip="Instagram"
-            id="instagram"
-          >
-            <Instagram />
-          </NavbarIconButton>
-          <NavbarIconButton
-            hideOnSmallDevices={true}
-            clickHandler={handleClick}
-            tooltip="GitHub"
-            id="github"
-          >
-            <GitHub />
-          </NavbarIconButton>
-          <NavbarIconButton
-            hideOnSmallDevices={false}
-            clickHandler={handleClick}
-            tooltip="Call me"
-            id="phone"
-          >
-            <Phone />
-          </NavbarIconButton>
-          <NavbarIconButton
-            hideOnSmallDevices={false}
-            clickHandler={handleClick}
-            tooltip="Message me"
-            id="text"
-          >
-            <Email />
-          </NavbarIconButton>
-        </Toolbar>
-      </AppBar>
+              <Menu />
+            </IconButton>
+            <Link to="home" smooth={true}>
+              <Typography
+                variant="h6"
+                variantMapping={{ h6: "h1" }}
+                style={{ cursor: "pointer" }}
+              >
+                {title}
+              </Typography>
+            </Link>
+            <span style={{ flex: 1 }} />
+            <NavbarIconButton
+              hideOnSmallDevices={true}
+              clickHandler={handleClick}
+              tooltip="Instagram"
+              id="instagram"
+            >
+              <Instagram />
+            </NavbarIconButton>
+            <NavbarIconButton
+              hideOnSmallDevices={true}
+              clickHandler={handleClick}
+              tooltip="GitHub"
+              id="github"
+            >
+              <GitHub />
+            </NavbarIconButton>
+            <NavbarIconButton
+              hideOnSmallDevices={false}
+              clickHandler={handleClick}
+              tooltip="Call me"
+              id="phone"
+            >
+              <Phone />
+            </NavbarIconButton>
+            <NavbarIconButton
+              hideOnSmallDevices={false}
+              clickHandler={handleClick}
+              tooltip="Message me"
+              id="text"
+            >
+              <Email />
+            </NavbarIconButton>
+          </Toolbar>
+        </AppBar>
+      </Fade>
       <CallMenu />
       <TextMenu />
     </>
