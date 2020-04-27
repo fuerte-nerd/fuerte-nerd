d5220e97df3d253b269f5ded4d2406c74ac889e4
David Andrews <dandrewsuk82@gmail.com>
2020-04-27 14:26:26 +0100

    Autogit: 27-04-2020 14:26

--- src/components/Reveal.js
+++ src/components/Reveal.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Fade } from "react-reveal"
 
 const Reveal = ({ children }) => {
-  return <Fade duration={100}>{children}</Fade>
+  return <Fade duration={600}>{children}</Fade>
 }
 
 export default Reveal
