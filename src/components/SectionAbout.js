13de371a604d3a842aab353bb92361498e4898eb
David Andrews <dandrewsuk82@gmail.com>
2020-04-27 13:35:37 +0100

    Autogit: 27-04-2020 13:35

--- src/components/SectionAbout.js
+++ src/components/SectionAbout.js
@@ -12,6 +12,7 @@ import {
 } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { GitHub, Instagram } from "@material-ui/icons"
+import { Fade } from "react-reveal"
 import SectionHeading from "./SectionHeading"
 import AboutExternalLink from "./AboutExternalLink"
 import ImageGrid from "./ImageGrid"
@@ -101,48 +102,71 @@ const SectionAbout = props => {
       <Container>
         <Grid container justify="center" alignItems="center" spacing={4}>
           <Grid item xs={12} md={7}>
-            <SectionHeading title="About" subtitle="Who am I?" />
-            <Typography variant="body1" className={classes.aboutText} paragraph>
-              I am a developer (primarily of the web variety!) originally from
-              England, now residing in the Canary Islands. I mainly work with{" "}
-              <strong>React</strong>, <strong>Gatsby</strong>, vanilla{" "}
-              <strong>JavaScript</strong> and <strong>Node</strong>, but I have
-              experience of working with numerous tech such as Django, Laravel,
-              Material UI, Bootstrap, PHP, Python (too many to mention them all
-              without sending you to sleep!)
-            </Typography>
-            <Typography variant="body2" className={classes.aboutText} paragraph>
-              I love my work but when I am not working, I enjoy volunteering at
-              the local dog rescue center, playing the piano and mountain-biking
-              around this beautiful island....although not necessarily at the
-              same time! :o)
-            </Typography>
-            <Divider />
+            <Fade>
+              <SectionHeading title="About" subtitle="Who am I?" />
+            </Fade>
+            <Fade left>
+              <Typography
+                variant="body1"
+                className={classes.aboutText}
+                paragraph
+              >
+                I am a developer (primarily of the web variety!) originally from
+                England, now residing in the Canary Islands. I mainly work with{" "}
+                <strong>React</strong>, <strong>Gatsby</strong>, vanilla{" "}
+                <strong>JavaScript</strong> and <strong>Node</strong>, but I
+                have experience of working with numerous tech such as Django,
+                Laravel, Material UI, Bootstrap, PHP, Python (too many to
+                mention them all without sending you to sleep!)
+              </Typography>
+            </Fade>
+            <Fade left>
+              <Typography
+                variant="body2"
+                className={classes.aboutText}
+                paragraph
+              >
+                I love my work but when I am not working, I enjoy volunteering
+                at the local dog rescue center, playing the piano and
+                mountain-biking around this beautiful island....although not
+                necessarily at the same time! :o)
+              </Typography>
+            </Fade>
+            <Fade left>
+              <Divider />
+            </Fade>
+
             <Box mt={3} align="center">
               <Grid container spacing={4} alignItems="center">
                 <Grid item xs={12} md={6}>
-                  <AboutExternalLink
-                    intro="If you're into code, check out..."
-                    icon={<GitHub />}
-                    clickHandler={handleClick}
-                    id="github"
-                    buttonText="My GitHub"
-                  />
+                  <Fade>
+                    <AboutExternalLink
+                      intro="If you're into code, check out..."
+                      icon={<GitHub />}
+                      clickHandler={handleClick}
+                      id="github"
+                      buttonText="My GitHub"
+                    />
+                  </Fade>
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <AboutExternalLink
-                    intro="If dogs are your thing, check out..."
-                    icon={<Instagram />}
-                    clickHandler={handleClick}
-                    id="instagram"
-                    buttonText="My Instagram"
-                  />
+                  <Fade>
+                    <AboutExternalLink
+                      intro="If dogs are your thing, check out..."
+                      icon={<Instagram />}
+                      clickHandler={handleClick}
+                      id="instagram"
+                      buttonText="My Instagram"
+                    />
+                  </Fade>
                 </Grid>
               </Grid>
             </Box>
           </Grid>
           <Grid item xs={12} md={5} style={{ overflow: "hidden" }}>
-            <ImageGrid />
+            <Fade>
+              <ImageGrid />
+            </Fade>
           </Grid>
         </Grid>
       </Container>
